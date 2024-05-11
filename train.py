ca2c8bc5ee46ebdf66a8a285e90c4973632f7d1e
Drew <160739722+drwwjm@users.noreply.github.com>
2024-05-12 02:02:11 +0800

    idk2

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 from ultralytics import YOLO
 
 if __name__ ==  "__main__":
-    model = YOLO('yolov8s.pt')
+    model = YOLO('yolov8m.pt')
+    torch_use_cuda_dsa = 1
 
-    results = model.train(data=r'D:\DRIVE\TUP Files\4A\Thesis\model\tcs\data.yaml', epochs=50, imgsz=640, project=r'D:\DRIVE\TUP Files\4A\Thesis\model\tcs\results', device=0, workers=0, optimize= True, lr0=0.001, lrf=0.01, fliplr=0.0,amp=False)
+    results = model.train(data=r'D:\DRIVE\TUP Files\4A\Thesis\model\tcs\data.yaml', epochs=50, imgsz=640, project=r'D:\DRIVE\TUP Files\4A\Thesis\model\tcs\results', device="CPU", lr0=0.001, lrf=0.01, fliplr=0.0)
